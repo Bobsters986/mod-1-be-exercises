171e62f10aea311fc82c487ca0250f3ded2cf885
Bobby Luly <bobbyluly@gmail.com>
2023-08-08 12:17:31 -0600

    Test: add total cost test

--- practice_independent_challenges/week5/reunion/spec/activity_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -30,4 +30,11 @@ RSpec.describe Activity do
     expect(activity.participants).to eq({"Maria" => 20, "Luther" => 40})
   end
 
+  it 'can calculate total cost' do
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+    activity.add_participant("Luther", 40)
+
+    expect(activity.total_cost).to eq(60)
+  end
 end
