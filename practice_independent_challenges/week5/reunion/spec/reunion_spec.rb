05055a9d419a832bf7a645a242f6b52b7a648599
Bobby Luly <bobbyluly@gmail.com>
2023-08-08 15:26:00 -0600

    Test: create test for #summary

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -65,4 +65,26 @@ RSpec.describe Reunion do
     expect(reunion.total_event_cost).to eq(180)
     expect(reunion.breakout).to eq({"Maria" => -10, "Luther" => -30, "Louis" => 40})
   end
+
+  it 'can calculate summary' do
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+
+    expect(reunion.total_event_cost).to eq(60)
+    expect(reunion.breakout).to eq({"Maria" => 10, "Luther" => -10})
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+
+    expect(reunion.total_event_cost).to eq(180)
+    expect(reunion.breakout).to eq({"Maria" => -10, "Luther" => -30, "Louis" => 40})
+
+    expect(reunion.summary).to eq("Maria: -10\nLuther: -30\nLouis: 40")
+  end
 end
